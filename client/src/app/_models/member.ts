9bbc728241e0247f69ee6267555d85a2b2255ee8
krishna-portfolio <nihao-1@outlook.com>
2021-08-26 19:28:29 +0530

    nueve ends.

---
+++ client/src/app/_models/member.ts
@@ -0,0 +1,18 @@
+import { photo } from "./Photo";
+
+export interface member {
+    id: number;
+    username: string;
+    photoUrl: string;
+    age: number;
+    knownAs: string;
+    created: Date;
+    lastActive: Date;
+    gender: string;
+    introduction: string;
+    lookingFor: string;
+    interests: string;
+    city: string;
+    country: string;
+    photos: photo[];
+}
