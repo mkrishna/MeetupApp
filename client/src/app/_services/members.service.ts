8e9bdc39726f6345951926a6513d43a00d41dbcd
krishna-portfolio <nihao-1@outlook.com>
2021-08-27 01:09:38 +0530

    diez ends.

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { member } from '../_models/member';
 
@@ -7,15 +9,31 @@ import { member } from '../_models/member';
   providedIn: 'root'
 })
 export class MembersService {
-  baseUrl = environment.apiUrl
+  baseUrl = environment.apiUrl;
+  members: member[] = [];
 
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<member[]>(this.baseUrl + 'users');
+    if(this.members.length > 0) return of(this.members);
+    return this.http.get<member[]>(this.baseUrl + 'users').pipe(
+      map(members => { this.members = members;
+        return this.members; })
+    );
   }
 
   getMember(username: string) {
+    const member = this.members.find(x => x.username === username);
+    if(member !== undefined) return of(member);
     return this.http.get<member>(this.baseUrl + 'users/' + username);
+  }
+
+  updateMember(member: member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    )
   }
 }
